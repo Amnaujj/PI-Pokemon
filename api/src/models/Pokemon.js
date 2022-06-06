035c5d26e3f604943fa556f5a7ddd3dda6300b88
juan cuevas <juannmcuevas97@gmail.com>
2022-06-06 15:02:06 -0300

    2nd Commit

--- api/src/models/Pokemon.js
+++ api/src/models/Pokemon.js
@@ -2,11 +2,46 @@ const { DataTypes } = require('sequelize');
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
-  // defino el modelo
-  sequelize.define('pokemon', {
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-  });
+// defino el modelo
+	sequelize.define('pokemon', {
+		id: {
+			type: DataTypes.UUID,
+            allowNull: false,
+            primaryKey: true,
+            defaultValue: DataTypes.UUIDV4
+		},
+		name: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		life: {
+			type: DataTypes.FLOAT,
+		},
+		atk: {
+			type: DataTypes.FLOAT,
+		},
+		def: {
+			type: DataTypes.FLOAT,
+		},
+		spd: {
+			type: DataTypes.FLOAT,
+		},
+		height: {
+			type: DataTypes.FLOAT,
+		},
+		weight: {
+			type: DataTypes.FLOAT,
+		}
+	}, {
+		timestamps: false,
+	});
 };
+
+// ID (Número de Pokemon) * : No puede ser un ID de un pokemon ya existente en la API pokeapi
+// Nombre *
+// Vida
+// Ataque
+// Defensa
+// Velocidad
+// Altura
+// Peso
