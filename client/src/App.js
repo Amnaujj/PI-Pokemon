2fe3c9a9f6afec57e95f57f5f673c338b2a86041
juan cuevas <juannmcuevas97@gmail.com>
2022-06-07 18:01:51 -0300

    8th Commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
+import { Route } from 'react-router-dom';
+
+import LandingPage from './components/landingPage/LandingPage.jsx';
+
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Henry Pokemon</h1>
-    </div>
-  );
+	return (
+		<div className="App">
+		
+		<Route exact path='/' component={LandingPage}/>
+		
+		</div>
+	);
 }
 
 export default App;
---
+++ client/src/components/landingPage/LandingPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './LandingPage.css'
+
+export default function LandingPage () {
+
+    return (
+        <div className="LandingPage">
+            <h1>hola</h1>
+            <button>START</button>
+        </div>
+    )
+}
